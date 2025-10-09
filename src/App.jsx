514fae176bb3aaadf026ec43683bb9eee764844e
adityatripathics22 <adityatripathics22@gmail.com>
2025-10-10 00:51:01 +0530

    Refactor App component to update user dynamically and fix rendering issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const App = () => {
   const a = 3;
+  let user = "aditya";
+
+  let changeUser = () => {
+    user = "harsh";
+  }
+
   return (
     <div>
-      <h1> hello {a}</h1>
-      <h2> hello 2</h2>
+      <h1> hello {user}</h1>
+      <button onClick={changeUser} > change user</button>
     </div>
   )
 }
