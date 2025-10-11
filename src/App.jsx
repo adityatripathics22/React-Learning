8b7a137051917934abdcc40f2bb55d5fdc85aa56
adityatripathics22 <adityatripathics22@gmail.com>
2025-10-12 00:24:00 +0530

    Remove unused CSS and refactor App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 
 const App = () => {
-  const a = 3;
-  let user = "aditya";
-
-  let changeUser = () => {
-    user = "harsh";
-  }
-
   return (
-    <div>
-      <h1> hello {user}</h1>
-      <button onClick={changeUser} > change user</button>
-    </div>
+    <div>App</div>
   )
 }
 
